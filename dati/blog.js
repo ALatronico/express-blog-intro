73dba8381d258089cbb38ea0299b29647aa1a5f7
ALatronico <gabant47@gmail.com>
2024-12-23 18:56:05 +0100

    ADD: app.js, images, html page

---
+++ dati/blog.js
@@ -0,0 +1,38 @@
+const bacheca = [
+  {
+    titolo: "ciambellone" ,
+    contenuto: "pippo" ,
+    immagine: "images/ciambellone.jpeg" , 
+    tags: [ '' ,'', '', ] 
+  },
+
+  {
+    titolo: "cracker" ,
+    contenuto: "paperino" ,
+    immagine: "images/cracker_barbabietola.jpeg" , 
+    tags: [ '' ,'', '', ] 
+  },
+
+  {
+    titolo: "pane" ,
+    contenuto: "topolino" ,
+    immagine: "images/pane_fritto_dolce.jpeg" , 
+    tags: [ '' ,'', '', ] 
+  },
+
+  {
+    titolo: "pasta" ,
+    contenuto: "minnie" ,
+    immagine: "images/pasta_barbabietola.jpeg" , 
+    tags: [ '' ,'', '', ] 
+  },
+
+  {
+    titolo: "torta" ,
+    contenuto: "pluto" ,
+    immagine: "images/torta_paesana.jpeg" , 
+    tags: [ '' ,'', '', ] 
+  }
+]
+
+module.exports = bacheca;
